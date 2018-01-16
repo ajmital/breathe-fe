e354f5e8637133ff9cf60298253755c3c5eadf77
Aman Mital <amital@nd.edu>
2018-01-15 23:40:32 -0500

    Added landing page, updated libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,18 +13,24 @@ import { FoodComponent } from './components/food/food.component';
 
 import { UserService } from './services/user.service';
 import { FoodService } from './services/food.service';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: LandingPageComponent }]
 
 @NgModule({
   declarations: [
     AppComponent,
     UserDataComponent,
     FoodComponent,
+    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
     NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [UserService, FoodService],
   bootstrap: [AppComponent]
